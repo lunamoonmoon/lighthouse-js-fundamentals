60a7ceaa2085404b278392e552f8473bbfdd4a4e
lunamoonmoon <5862lunamoon@gmail.com>
2023-05-28 21:42:46 +0000

    file of ice cream flavours

---
+++ ice-cream.js
@@ -0,0 +1,7 @@
+const iceCreamFlavours = ["chocolate", "vanilla", "cookies and cream", "rocky road", "strawberry"]; //creates variable array of flavours
+console.log(iceCreamFlavours);//prints array to console
+iceCreamFlavours.push("root beer");
+console.log(iceCreamFlavours);
+console.log(iceCreamFlavours[0]);
+console.log(iceCreamFlavours[iceCreamFlavours.length - 1]);
+console.log(iceCreamFlavours.length);
